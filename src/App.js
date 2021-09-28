626e3d38968c200257d8db329cab482278e2d7ac
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-09-27 23:23:07 -0400

    Add Book component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>Bookstore</h1>
-      </nav>
+      <Router>
+        <nav className="navbar">
+          <h1>Bookstore</h1>
+          <Link to="/books">BOOKS</Link>
+          <Link to="/categories">CATEGORIES</Link>
+        </nav>
+        <Switch>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/redux/books/books.js
@@ -0,0 +1,13 @@
+const Books = () => (
+  <main className="catalog">
+    <div className="item">
+      <h2 className="book-title">Macbeth</h2>
+      <h3 className="book-author">William Shakespeare</h3>
+      <div className="book-links">
+          <a>Comments</a>
+          <a>Remove</a>
+          <a>Edit</a>
+      </div>
+    </div>
+  </main>
+);
