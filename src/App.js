40379c71d2000808da37f223d99caf3977dfc932
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-09-28 15:06:22 -0400

    Attempt to fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
-import Books from "./components/books";
-import Categories from "./components/categories";
+  Link,
+} from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
 import './App.css';
 
 function App() {
@@ -15,9 +15,9 @@ function App() {
       <Router>
         <nav className="navbar">
           <div className="nav-elements">
-          <h1>Bookstore</h1>
-          <Link to="/books" className="navlink">BOOKS</Link>
-          <Link to="/categories" className="navlink">CATEGORIES</Link>
+            <h1>Bookstore</h1>
+            <Link to="/books" className="navlink">BOOKS</Link>
+            <Link to="/categories" className="navlink">CATEGORIES</Link>
           </div>
         </nav>
         <Switch>
--- src/components/books.js
+++ src/components/books.js
@@ -2,28 +2,28 @@ const Books = () => (
   <main className="catalog">
     <div className="item">
       <div className="item-left">
-      <div className="book-details">
-      <h5 className="book-genre">TRAGEDY</h5>
-      <h2 className="book-title">Macbeth</h2>
-      <h3 className="book-author">William Shakespeare</h3>
-      </div>
-      <div className="book-links">
+        <div className="book-details">
+          <h5 className="book-genre">TRAGEDY</h5>
+          <h2 className="book-title">Macbeth</h2>
+          <h3 className="book-author">William Shakespeare</h3>
+        </div>
+        <div className="book-links">
           <a className="book-link" href="/#">Comments</a>
           <a className="book-link" href="/#">Remove</a>
           <a className="book-link" href="/#">Edit</a>
+        </div>
       </div>
-    </div>  
       <div className="book-progress">
         <h1 className>60%</h1>
       </div>
       <div className="item-right">
         <div className="chapter-details">
-        <div className="chapter-heading">CURRENT CHAPTER</div>
-        <div className="chapeter-name">Chapter 3</div>
+          <div className="chapter-heading">CURRENT CHAPTER</div>
+          <div className="chapeter-name">Chapter 3</div>
         </div>
-        <button>UPDATE PROGRESS</button>
+        <button type="button">UPDATE PROGRESS</button>
       </div>
-      </div>
+    </div>
   </main>
 );
 
